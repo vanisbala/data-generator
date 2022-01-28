a38adfb8de56b9a8ab92313b131985ec4149e419
vani <vanisbala@gmail.com>
2022-01-27 20:55:39 -0800

    added all the packages

--- http_server.js
+++ http_server.js
@@ -5,6 +5,7 @@ const fs = require('lowdb/adapters/FileSync');
 const adapter = new fs('db.json');
 const db = low(adapter);
 const cors = require('cors');
+const dotenv = require('dotenv');
 
 // allow cross-origin resource sharing (CORS)
 app.use(cors());
